310ce54bd04744e9c088b433a362e0ce5f6fbc8b
Alexandr Tichon <tsikhan.p@gmail.com>
2018-10-27 23:11:36 +0300

    feat: test; fix: test bug

---
+++ test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+Object.freeze(assert);
+const check = require('./src/sumOfOther.js');
+
+it('should return array consict of 4 elemenst, there are [8, 7, 6, 9]', () => {
+    assert.deepEqual(check([2, 3, 4, 1]), [8, 7, 6, 9]);
+});
